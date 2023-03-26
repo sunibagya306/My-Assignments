fed620d6bf461285a954544e2182ed09f48297e3
sunibagya306 <128947172+sunibagya306@users.noreply.github.com>
2023-03-26 10:36:58 +0800

    Add files via upload
    day1

---
+++ day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version=100.2f;
+		String name="firefox";
+		boolean visible=true;
+		int year=1998;
+		char logo='m';
+		System.out.println(version);
+		System.out.println(logo);
+	    System.out.println("name of the " +name + " visible is "+visible + " year is "+year);
+	    
+
+	}
+
+}
---
+++ day1/Mobile.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Mobile {
+	public void sendSms() {
+		System.out.println("From OPPO");
+	}
+	public void makeCall() {
+		System.out.println("Calling my friend");
+	}
+
+	public static void main(String[] args) {
+		Mobile mob =new Mobile();
+		mob.sendSms();
+		mob.makeCall();
+		
+		
+
+	}
+
+}
