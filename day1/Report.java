842bf8764e3189ff4b4b3b0a7e221c549f7444ca
sunibagya306 <128947172+sunibagya306@users.noreply.github.com>
2023-03-30 17:36:12 +0800

    Add files via upload

---
+++ day1/Report.java
@@ -0,0 +1,26 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student suneetha = new Student();
+		String studentName = suneetha.studentName;
+		int rollNO = suneetha.rollNo;
+		String collageName = suneetha.collageName;
+		short markScored = suneetha.markScored;
+		float cpga =suneetha.cgpa;
+		System.out.println("Student Name : " +studentName);
+		System.out.println("Roll No : " +rollNO);
+		System.out.println("Collage Name : " +collageName);
+		System.out.println("Mark Scored : " +markScored);
+		System.out.println("cgpa : " +cpga);
+		
+		
+		
+
+	}
+
+}
+
---
+++ day1/Student.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Student {
+	String studentName = "Suneetha";
+    int rollNo = 306;
+	String collageName = "Adithya Collage";
+	short markScored = 81;
+	float cgpa = 8.1f;
+	
+			
+
+	
+
+}
