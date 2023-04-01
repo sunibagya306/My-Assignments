7daf86409813afbd34c8c88218fb6c1bc314baae
sunibagya306 <128947172+sunibagya306@users.noreply.github.com>
2023-04-01 16:57:34 +0800

    Add files via upload

---
+++ week2day2/LaunchBrowser.java
@@ -0,0 +1,16 @@
+package week2day2;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.facebook.com/login");
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+
+	}
+
+}
